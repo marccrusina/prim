dd5ac45cd30080f179b4c473566966276d499b53
Zaldivar <ozaldivar@transitions.com>
2017-06-29 21:56:34 +0800

    updated prim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { RouterModule } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { TimeInComponent } from './time-in/time-in.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes = [
+  { path: '', redirectTo: "login", pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'time-in', component: TimeInComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TimeInComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) //routes
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/time-in/time-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-in',
+  templateUrl: './time-in.component.html',
+  styleUrls: ['./time-in.component.css']
+})
+export class TimeInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
